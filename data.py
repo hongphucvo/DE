e2ff77f33fa1424afe39fc98aae4be7a27495365
hongphucvo <phuc.vo_khmt2019@hcmut.edu.vn>
2022-05-07 01:22:22 +0700

    bug: unclean m1 outcome

--- data.py
+++ data.py
@@ -7,5 +7,5 @@ for i in range (1,size +1):
 
     url = 'https://www.gutenberg.org/cache/epub/'+str(i)+'/pg'+str(i)+'.txt.utf8'
     r = requests.get(url, allow_redirects=True)
-    filename = './collection/'+str(i)+'.txt'
+    filename = './col/'+str(i)+'.txt'
     open(filename, 'wb').write(r.content)
